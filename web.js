776e2833347d22c81cb1d545d6202ac4b5424bb6
jaigurgon <jaigurdiel@gmail.com>
2013-07-11 21:05:35 +0200

    web.js modified to read index.html file debug1

--- web.js
+++ web.js
@@ -2,15 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var data = new Buffer('data')
 
-fs.readFileSync('/etc/passwd', function (err, data) {
+fs.readFileSync('/index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
 
 app.get('/', function(request, response) {
-  response.send(data.toString());
+  response.send(console.toString());
 });
 
 var port = process.env.PORT || 5000;
